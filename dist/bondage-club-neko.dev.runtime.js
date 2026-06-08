1423a8659d7d6bd5b94f54ef2b236e210bcc3e59
moon <3012546069@qq.com>
2026-06-09 02:51:40 +0800

    Hide soft paws when neko mode is disabled

--- dist/bondage-club-neko.dev.runtime.js
+++ dist/bondage-club-neko.dev.runtime.js
@@ -3160,6 +3160,10 @@
         overflow: hidden;
       }
 
+      body:not(.bcn-enabled) #bcn-soft-paws {
+        display: none !important;
+      }
+
       #bcn-soft-paws span {
         position: absolute;
         opacity: 0.14;
